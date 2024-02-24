df50fd9f9cde3e98172002282ea56c5078238a61
aadrikasingh18 <aadrikas16@gmail.com>
2024-02-24 19:46:05 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
+import React from 'react';
+import { useState } from 'react';
 import './App.css';
+import './Scss/index.css'
+import PeopleList from './Components/PeopleList';
+import PlanetList from './Components/PlanetList';
 
 function App() {
+  const [peopleId, setCharacter] = useState(null)
+  const [planetId, setPlanet] = useState(null)
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className="container">
+      <h1 className="text-center">Star Wars</h1>
+      {peopleId || planetId
+        ? (
+          <>
+            <button className="button primary inline" onClick={() => { setCharacter(null); setPlanet(null) }} type="button">
+              &lsaquo; Return to Main
+            </button>
+            <p>People or Planet Details</p>
+          </>
+        )
+        : (
+          <>
+            <h2>People</h2>
+            <div className="horizontal-list">
+              <PeopleList setCharacter={setCharacter} />
+            </div>
+            <h2>Planets</h2>
+            <div className="vertical-list">
+              <PlanetList setPlanet={setPlanet} />
+            </div>
+          </>
+        )}
+      <footer>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Star Wars and all associated names and/or images are copyright Lucasfilm Ltd.
+          Images taken from &nbsp;
+          <a href="https://starwars-visualguide.com/" target="_blank" rel="noreferrer">starwars-visualguide.com</a>
+          .
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </footer>
     </div>
-  );
+  )
 }
 
 export default App;
